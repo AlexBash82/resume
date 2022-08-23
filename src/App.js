7c31764a9695ab162c7887d530da3bf7126b5a4f
AlexBash82 <kometa.4m@gmail.com>
2022-08-23 21:41:27 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
 
 function App() {
+  function fullYear() {
+    const dob = '19820219'
+    const year = Number(dob.substr(0, 4))
+    const month = Number(dob.substr(4, 2)) - 1
+    const day = Number(dob.substr(6, 2))
+    const today = new Date()
+    let age = today.getFullYear() - year
+    if (
+      today.getMonth() < month ||
+      (today.getMonth() === month && today.getDate() < day)
+    ) {
+      age--
+    }
+    return age
+  }
+
+  const skills = [
+    'JavaScript',
+    'React',
+    'TypeScript',
+    'Redux',
+    'SASS',
+    'Flex',
+    'Grid',
+    'GitHub',
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Body">
+      <div className="Body_resume">
+        <div className="Body_resume_title">Junior Frontend Developer</div>
+        <div className="Body_resume_mainInfo">
+          <div className="Body_resume_mainInfo_phrame">
+            <div className="Body_resume_mainInfo_phrame_photo"></div>
+          </div>
+          <div className="Body_resume_mainInfo_text">
+            <div className="Body_resume_mainInfo_text_title">
+              Башктов Алексей Сергеевич
+            </div>
+            <div className="Body_resume_mainInfo_text_string">
+              Локация: Екатеринбург
+            </div>
+            <div className="Body_resume_mainInfo_text_string">
+              Полных лет: {fullYear()}
+            </div>
+          </div>
+        </div>
+        <div className="Body_resume_personInfo">
+          <div className="Body_resume_personInfo_skills">
+            <div className="Body_resume_personInfo_skills_title">Навыки</div>
+            <ul>
+              {skills.map((item, index) => {
+                return (
+                  <li>
+                    <div
+                      key={index}
+                      className="Body_resume_personInfo_skills_item"
+                    >
+                      {item}
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className="Body_resume_personInfo_variants">
+            <div className="Body_resume_personInfo_variants_title">
+              Список работ
+            </div>
+            <ul>
+              <li>
+                <div className="Body_resume_personInfo_variants_item">
+                  Лендинг для компании{' '}
+                  <a href="https://alexbash82.github.io/website-bitafit/">
+                    BitaFit
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
