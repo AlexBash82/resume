fe609c29c819a81e7d2ff561afe75e6c0d97d80c
AlexBash82 <kometa.4m@gmail.com>
2022-09-05 20:55:08 +0500

    примеры работ, ссылки на гит, фотка

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
     'Flex',
     'Grid',
     'GitHub',
+    'Bootstrap',
   ]
 
   return (
@@ -46,6 +47,12 @@ function App() {
             <div className="Body_resume_mainInfo_text_string">
               Полных лет: {fullYear()}
             </div>
+            <div className="Body_resume_mainInfo_text_string">
+              Почта: kometa.4m@gmail.com
+            </div>
+            <div className="Body_resume_mainInfo_text_string">
+              Телефон: 8-901-201-701-восемь
+            </div>
           </div>
         </div>
         <div className="Body_resume_personInfo">
@@ -54,11 +61,8 @@ function App() {
             <ul>
               {skills.map((item, index) => {
                 return (
-                  <li>
-                    <div
-                      key={index}
-                      className="Body_resume_personInfo_skills_item"
-                    >
+                  <li key={index}>
+                    <div className="Body_resume_personInfo_skills_item">
                       {item}
                     </div>
                   </li>
@@ -73,12 +77,56 @@ function App() {
             <ul>
               <li>
                 <div className="Body_resume_personInfo_variants_item">
-                  Лендинг для компании{' '}
-                  <a href="https://alexbash82.github.io/website-bitafit/">
-                    BitaFit
-                  </a>{' '}
-                  . В проекте применены: JS, SASS...
+                  <p className="Body_resume_personInfo_variants_item_name">
+                    Лендинг для компании{' '}
+                    <a href="https://alexbash82.github.io/website-bitafit/">
+                      BitaFit
+                    </a>
+                  </p>
+                  <p className="Body_resume_personInfo_variants_item_text">
+                    Сайт адаптирован под различные экраны: от 320рх до 1920рх.
+                  </p>
+                  <p className="Body_resume_personInfo_variants_item_text">
+                    В проекте применены: JS, React, SASS, Flex, React-hook-form,
+                    <br />
+                    React-input-mask.
+                  </p>
+                  <p className="Body_resume_personInfo_variants_item_text">
+                    Ссылка на репозиторий{' '}
+                    <a href="https://github.com/AlexBash82/website-bitafit">
+                      GitHub
+                    </a>
+                  </p>
+                  <p className="Body_resume_personInfo_variants_item_text">
+                    Ссылка на{' '}
+                    <a href="https://alexbash82.github.io/website-bitafit/">
+                      Github pages
+                    </a>
+                  </p>
                 </div>
+              </li>
+              <li>
+                <p className="Body_resume_personInfo_variants_item_name">
+                  Логическая игра "Пятнашки"
+                </p>
+                <p className="Body_resume_personInfo_variants_item_text">
+                  Небольшой Pet-project, основной целью которого является
+                  демострация работы с библиотекой Redux.
+                </p>
+                <p className="Body_resume_personInfo_variants_item_text">
+                  В проекте применены: JS, React, SASS, Grid, Flex, Redux, а так
+                  же кастомный хук useReAssign()
+                </p>
+                <p className="Body_resume_personInfo_variants_item_text">
+                  Ссылка на репозиторий{' '}
+                  <a href="https://github.com/AlexBash82/fifteen">GitHub</a>
+                </p>
+                <p className="Body_resume_personInfo_variants_item_text">
+                  Ссылка на{' '}
+                  <a href="https://alexbash82.github.io/fifteen/">
+                    Github pages
+                  </a>
+                </p>
               </li>
             </ul>
           </div>
